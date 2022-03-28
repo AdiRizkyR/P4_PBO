3baa7967569d3a2da8169a3cb883e911d96f422e
AdiRizkyR <adirizkyramadhan912@gmail.com>
2022-03-28 14:17:52 +0700

    program mengakses anggota kelas(member class)

--- src/A_Percobaan01.java
+++ src/A_Percobaan01.java
@@ -1,10 +1,12 @@
 public class A_Percobaan01 {
+    //class global
     public int nrp;
     public String nama;
     public void info (){
         System.out.println("Ini siswa RPL Polbeng");
     }
 
+    //eksekusi
     public static void main(String[] args) {
         A_Percobaan01 it = new A_Percobaan01();
         it.nrp = 5;
