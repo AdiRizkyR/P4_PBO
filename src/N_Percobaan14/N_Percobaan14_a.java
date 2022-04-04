dec7336b101ed1d117af1cf7ed051c9cd82ed5dd
AdiRizkyR <adirizkyramadhan912@gmail.com>
2022-04-04 15:06:28 +0700

    Program java, metode constructor

---
+++ src/N_Percobaan14/N_Percobaan14_a.java
@@ -0,0 +1,28 @@
+package N_Percobaan14;
+
+public class N_Percobaan14_a {
+
+    String nama;
+    int umur;
+    String warnaRambut;
+    String warnaKulit;
+    int tinggiBadan;
+    int beratBadan;
+
+    public N_Percobaan14_a(){
+    }
+
+    public N_Percobaan14_a(String nama, int umur){
+        this.nama = nama;
+        this.umur = umur;
+    }
+
+    public void menangis(){
+        System.out.println("hu....hu....");
+    }
+
+    public void tertawa(){
+        System.out.println("ha...ha...");
+    }
+}
+
