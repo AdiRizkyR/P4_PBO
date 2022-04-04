5ac07d423b9fef58751294db5ca2a89c1472baa6
AdiRizkyR <adirizkyramadhan912@gmail.com>
2022-04-04 15:05:04 +0700

    Program java, metode dengan nilai kembali yang memiliki parameter

---
+++ src/L_Percobaan12/L_Percobaan12.java
@@ -0,0 +1,12 @@
+package L_Percobaan12;
+
+public class L_Percobaan12 {
+    public static void main(String[] args) {
+        double hasilBagi = bagi(9,3);
+        System.out.println("Hasil Bagi = " + hasilBagi);
+    }
+
+    private static double bagi(double param1, double param2){
+        return (param1/param2);
+    }
+}
