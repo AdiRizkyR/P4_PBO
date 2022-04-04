30b74aa41b031185e9cdf91172e2566531475336
AdiRizkyR <adirizkyramadhan912@gmail.com>
2022-04-04 15:02:28 +0700

    Program java, metode tanpa nilai kembali(void)

---
+++ src/I_Percobaan09/I_Percobaan09.java
@@ -0,0 +1,11 @@
+package I_Percobaan09;
+
+public class I_Percobaan09 {
+    public static void main(String[] args) {
+        welcome();
+    }
+
+    private static void welcome(){
+        System.out.println("Selamat Belajar Java");
+    }
+}
