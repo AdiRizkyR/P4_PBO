968c6f89f290c48c619cf7f9dc89a0997c8e4870
AdiRizkyR <adirizkyramadhan912@gmail.com>
2022-04-04 14:57:05 +0700

    Program java untuk mengimplementasikan class diagram (UML) menjadi kode sumber

---
+++ src/D_Percobaan04/D_Percobaan04.java
@@ -0,0 +1,36 @@
+package D_Percobaan04;
+
+public class D_Percobaan04 {
+    private String nama;
+    private int nrp;
+
+    public String getNama(){
+        return nama;
+    }
+
+    public void setNama(String nama){
+        this.nama = nama;
+    }
+
+    public int getNrp(){
+        return nrp;
+    }
+
+    public void setNrp(int nrp){
+        this.nrp = nrp;
+    }
+
+    public D_Percobaan04(String nama, int nrp){
+        this.nama = nama;
+        this.nrp = nrp;
+    }
+
+    public void info(){
+        System.out.println(nama + ", " + nrp);
+    }
+
+    public String toString(){
+        System.out.println("Data Mahasiswa\n" + nama + "\n" + nrp);
+        return null;
+    }
+}
