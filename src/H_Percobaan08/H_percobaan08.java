aaa9e60a922d102606416115a135c5a33dac325d
AdiRizkyR <adirizkyramadhan912@gmail.com>
2022-04-04 15:01:35 +0700

    Program java menggunakan keyword final

---
+++ src/H_Percobaan08/H_percobaan08.java
@@ -0,0 +1,14 @@
+package H_Percobaan08;
+
+public class H_percobaan08 {
+
+    int x = 10;
+    double PI = 3.14;
+
+    public static void main(String[] args) {
+        H_percobaan08 myObj = new H_percobaan08();
+        myObj.x = 50;
+        myObj.PI = 25;
+        System.out.println(myObj.x);
+    }
+}
